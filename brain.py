1158988abd153bdd8b54ce23c834e07f1932c109
RaghavVashisth <raghavvashistha8869@gmail.com>
2026-01-30 12:22:28 +0530

    second comit

--- brain.py
+++ brain.py
@@ -1648,8 +1648,9 @@ elif selected_screen == "Monitoring Dashboard":
     st.markdown("#### Embedded Power BI Dashboard Below:")
 
     powerbi_embed_url = """
-    <iframe title="Litigation Propensity Model Monitoring Dashboard" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=1cd0509f-2f9e-4d7a-8bdd-0094e3c65623&autoAuth=true&ctid=dafe49bc-5ac3-4310-97b4-3e44a28cbf18%22 frameborder="0" allowFullScreen="true"></iframe>
- 
+ <iframe title="Litigation Propensity Model Monitoring Dashboard" width="1140" height="541.25" src=https://app.powerbi.com/reportEmbed?reportId=1cd0509f-2f9e-4d7a-8bdd-0094e3c65623&autoAuth=true&ctid=dafe49bc-5ac3-4310-97b4-3e44a28cbf18
+ frameborder="0" allowFullScreen="true"></iframe>
+
     """
     componentspip.html(powerbi_embed_url, height=650)
 
@@ -2111,8 +2112,9 @@ elif selected_screen == "Legal Spend Dashboard":
     st.markdown("#### Embedded Power BI Dashboard Below:")
 
     powerbi_embed_url = """
-    <iframe title="Legal Expense Dashboard" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=dbc06794-896c-494e-a901-caa07670f375&autoAuth=true&ctid=dafe49bc-5ac3-4310-97b4-3e44a28cbf18%22 frameborder="0" allowFullScreen="true"></iframe>
- 
+ <iframe title="Legal Expense Dashboard" width="1140" height="541.25" src=https://app.powerbi.com/reportEmbed?reportId=dbc06794-896c-494e-a901-caa07670f375&autoAuth=true&ctid=dafe49bc-5ac3-4310-97b4-3e44a28cbf18
+ frameborder="0" allowFullScreen="true"></iframe>
+
     """
     componentspip.html(powerbi_embed_url, height=650)
 
